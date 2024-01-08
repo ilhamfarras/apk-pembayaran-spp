0864510066f3039564b77f12a04f739bd51ece6f
ilhamfarras <117500149+ilhamfarras@users.noreply.github.com>
2024-01-08 11:06:48 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+
+<footer class="footer">
+      <div class="container">
+        <p class="text-muted">Created By kelompok Pembayaran SPP</p>
+      </div>
+    </footer>
+    <script src="js/jquery.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
